03eac7dfce0ee77b5b2feeeea6d6ecfee82e3f10
Jerandom <89083263+Jerandom@users.noreply.github.com>
2025-04-16 16:30:25 +0800

    added functions

--- MyCPlusPlusCmakeProject/CSharp/DDS/DDSListener.cs
+++ MyCPlusPlusCmakeProject/CSharp/DDS/DDSListener.cs
@@ -14,7 +14,7 @@ public class DDSListener<T> : DDS.DataReaderListener
     }
 
     // Event
-    public event Action<T> onDataRecievedl
+    public event Action<T> onDataRecieved;
 
     private object data_seq;
     private DDS.SampleInfoSeq info_seq;
@@ -43,5 +43,88 @@ public class DDSListener<T> : DDS.DataReaderListener
             // assign DDS Reader to this object
             DDSState.Instance.ddsDataReader[$"{typeof(T).FullName}"].set_listener(this, DDS.StatusMask.STATUS_MASK_ALL);
         });
+    }
+
+    public override void on_requested_deadline_missed(
+        DDS.DataReader reader,
+        ref DDS.RequestedDeadlineMissedStatus status)
+    { }
+
+    public override void on_requested_incompatible_qos(
+        DDS.DataReader reader,
+        DDS.RequestedIncompatibleQosStatus status)
+    { }
+
+    public override void on_sample_rejected(
+        DDS.DataReader reader,
+        ref DDS.SampleRejectedStatus status)
+    { }
+
+    public override void on_liveliness_changed(
+        DDS.DataReader reader,
+        ref DDS.LivelinessChangedStatus status)
+    { }
+
+    public override void on_sameple_lost(
+        DDS.DataReader reader,
+        ref DDS.SampleLostStatus status)
+    { }
+
+    public override void on_subscription_match(
+        DDS.DataReader reader,
+        ref DDS.SubscriptionMatchStatus status)
+    { }
+
+    public override void on_data_available(DDS.DataReader reader)
+    {
+        int counter = 1;
+        object T_reader = null;
+
+        // check if the event is registered, delay the code execution if no event is registered
+        while(onDataRecieved == null && counter <= 5)
+        {
+            Console.Writeline($"No Subscribers for {typeof(T).FullName}, Delaying DataReader...{counter}s");
+            Thread.Sleep(1000);
+            counter++;
+        }
+
+        if(onDataRecieved == null)
+        {
+            Console.Writeline($"Still no subscribers for {typeof(T).FullName}");
+            return;
+        }
+
+        // take data for reading
+        try
+        {
+            // get and convert the object
+            T_reader = DDSState.Instance.ddsDataReader[$"{typeof(T).FullName}"];
+
+            // get the take() method
+            MethodInfo takeMethod = T_reader.GetType.GetMethod("take", BindingFlags.Instance | BindingFlags.Public);
+            if(takeMethod == null)
+            {
+                Console.Writeline($"The Class {T_reader.GetType().Name} does not have a instance take() method");
+                return;
+            }
+
+            // Invoke take()
+            takeMethod.Invoke(T_reader, new object[] { data_seq,
+                                                       info_seq,
+                                                       DDS.ResourceLimitsQosPolicy.LENGTH_UNLIMITED,
+                                                       (uint)DDS.SampleStateKind.ANY_SAMPLE_STATE,
+                                                       (uint)DDS.ViewStateKind.ANY_VIEW_STATE,
+                                                       (uint)DDS.InstanceStateKind.ANY_INSTANCE_STATE });
+        }
+        catch (DDS.Retcode_NoData)
+        {
+            Console.Writeline($"NoData exception on DDS recieve for {typeof(T).FullName}");
+            return;
+        }
+        catch (DDS.Exception)
+        {
+            Console.Writeline($"Error on take data in {typeof(T).FullName}");
+            return;
+        }
     }
 }
